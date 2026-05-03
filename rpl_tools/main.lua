5ed4fa48b62dbd0af96df860df5020d856b41b0a
zJoopez <joope.vesa@gmail.com>
2026-05-03 06:00:07 +0300

    colors for chat messages added

--- rpl_tools/main.lua
+++ rpl_tools/main.lua
@@ -12,8 +12,9 @@ local inReplayMode = false
 
 --functions
 
-local function custom_echo(msg)
-    echo("^28[ReplayFocus] ^41" .. msg)
+local function custom_echo(msg, color)
+    if color < 10 then color = "0"..color end
+    echo("^".. COLORS.BLOSSOM .."[ReplayFocus] ^"..color .. msg)
 end
 
 local function rewindIfOutOfBounds()
@@ -25,14 +26,14 @@ local function rewindIfOutOfBounds()
     local currentFrame = ws.match_frame or 0
     if currentFrame < replayBounds.startFrame or currentFrame > replayBounds.endFrame then
         rewind_replay_to_frame(replayBounds.startFrame)
-        custom_echo("Replay out of bounds at frame " .. currentFrame .. ", rewinding to " .. replayBounds.startFrame)
+        custom_echo("Replay out of bounds at frame " .. currentFrame .. ", rewinding to " .. replayBounds.startFrame, COLORS.VIOLET)
     end
 end
 
 --These run on start
 if get_option("replaycache") < 1 then set_option("replaycache", 2) end 
 
-custom_echo("Activating Replay monitoring")
+custom_echo("Activating Replay monitoring", COLORS.GREEN)
 add_hook("enter_frame", "keepReplayWithinBounds", function()
     local ws = get_world_state()
     if ws.replay_mode ~= 0 then
